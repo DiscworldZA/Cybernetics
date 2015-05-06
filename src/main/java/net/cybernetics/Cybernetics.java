b63d48224c88a1ae3d0db79a79c2e3f74fe71338
sekwah41 <alastair.hawkes@gmail.com>
2015-05-06 07:27:32 +0100

    Added base Extendedproperties file and also the proxy

--- src/main/java/net/cybernetics/Cybernetics.java
+++ src/main/java/net/cybernetics/Cybernetics.java
@@ -1,16 +1,26 @@
 package net.cybernetics;
 
-import net.cybernetics.block.CybBlocks;
-import net.cybernetics.item.CybItems;
-import net.cybernetics.ref.ModRef;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.cybernetics.block.CybBlocks;
+import net.cybernetics.generic.CommonProxy;
+import net.cybernetics.item.CybItems;
+import net.cybernetics.ref.ModRef;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(modid = ModRef.MODID, name = ModRef.MODNAME, version = ModRef.VERSION, dependencies = "")
 public class Cybernetics
-{	
+{
+
+    public static final Logger LOGGER = LogManager.getLogger("Cybernetics");
+
+    @SidedProxy(clientSide = "sekwah.mods.narutomod.client.ClientProxy", serverSide = "sekwah.mods.narutomod.generic.CommonProxy")
+    public static CommonProxy proxy;
+
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event)
 	{
